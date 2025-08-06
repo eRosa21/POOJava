5dc81401ef1eb985c8d2686c42df4a8622505130
eRosa21 <rosaesthefano@gmail.com>
2025-08-05 21:05:37 -0300

    EXEMPLO SLIDE 1

---
+++ exemplo1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+
+        System.out.println("teste");
+        System.out.println("O Valor de z é: " + (x + y));
+
+
+    }
+}
